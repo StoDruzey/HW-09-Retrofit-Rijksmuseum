925cae85dd6472f1edfcb9c9ec5d610bdb983e21
Serge Labada <sergelabada@gmail.com>
2022-09-09 18:22:57 +0300

    Got a list of art objects

--- app/src/main/java/com/example/hw09retrofitrijksmuseum/RetrofitFragment.kt
+++ app/src/main/java/com/example/hw09retrofitrijksmuseum/RetrofitFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import coil.load
@@ -20,7 +21,9 @@ class RetrofitFragment : Fragment() {
     private var _binding: FragmentRetrofitBinding? = null
     private val binding get() = requireNotNull(_binding)
 
-    //    private var currentRequest: Call<List<ArtObject>>? = null
+    private val adapter by lazy { ArtAdapter(requireContext()) }
+
+    private var artObjectRequest: Call<ArtObject>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,14 +38,9 @@ class RetrofitFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor { chain ->
-                val newRequest = chain.request().newBuilder()
-                    .addHeader("key", "ZOavwPKX")
-                    .build()
-                chain.proceed(newRequest)
-            }
-            .build()
+        with(binding) {
+            recyclerView.adapter = adapter
+        }
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://www.rijksmuseum.nl/")
@@ -52,51 +50,44 @@ class RetrofitFragment : Fragment() {
 
         val rijksmuseumApi = retrofit.create<RijksmuseumAPI>()
 
-        rijksmuseumApi
+        artObjectRequest = rijksmuseumApi
             .getArtObject("Rembrandt van Rijn", 100, "ZOavwPKX")
-            .enqueue(object : Callback<ArtObject> {
-                override fun onResponse(call: Call<ArtObject>, response: Response<ArtObject>) {
-                    println()
-                }
+            .apply {
+                enqueue(object : Callback<ArtObject> {
+                    override fun onResponse(call: Call<ArtObject>, response: Response<ArtObject>) {
+                        if (response.isSuccessful) {
+                            val artList = response.body()?.artObjects ?: return
+                            adapter.submitList(artList)
+                        } else {
+                            handleException(HttpException(response))
+                        }
+                    }
 
-                override fun onFailure(call: Call<ArtObject>, t: Throwable) {
-                    println()
-                }
-            })
-
-//        currentRequest = rijksmuseumApi
-//            .getArtObject()
-//            .apply {
-//                enqueue(object : Callback<List<ArtObject>> {
-//                    override fun onResponse(call: Call<List<ArtObject>>, response: Response<List<ArtObject>>) {
-//                        if (response.isSuccessful) {
-//                            val artObject = response.body() ?: return
-//                            binding.imageView.load(artObject[1].url)
-//                        } else {
-//                            handleException(HttpException(response))
-//                        }
-//                    }
-//
-//                    override fun onFailure(call: Call<List<ArtObject>>, t: Throwable) {
-//                        if (!call.isCanceled) {
-//                            handleException(t)
-//                        }
-//                    }
-//                })
-//            }
-
-        with(binding) {
-
-        }
+                    override fun onFailure(call: Call<ArtObject>, t: Throwable) {
+                        if (!call.isCanceled) {
+                            handleException(t)
+                        }
+                    }
+                })
+            }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-//        currentRequest?.cancel()
+        artObjectRequest?.cancel()
         _binding = null
     }
 
-    private fun handleException(t: Throwable) {
-
+    private fun handleException(e: Throwable) {
+        Toast.makeText(requireContext(), e.message ?: "Something went wrong", Toast.LENGTH_SHORT).show()
     }
 }
+
+//        val okHttpClient = OkHttpClient.Builder()
+//            .addInterceptor { chain ->
+//                val newRequest = chain.request().newBuilder()
+//                    .addHeader("key", "ZOavwPKX")
+//                    .build()
+//                chain.proceed(newRequest)
+//            }
+//            .build()
