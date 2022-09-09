bf59883e70dc0f94460f497719215f868aa7bab3
Serge Labada <sergelabada@gmail.com>
2022-09-10 00:40:06 +0300

    Adding a search in list

--- app/src/main/java/com/example/hw09retrofitrijksmuseum/RetrofitFragment.kt
+++ app/src/main/java/com/example/hw09retrofitrijksmuseum/RetrofitFragment.kt
@@ -2,13 +2,16 @@ package com.example.hw09retrofitrijksmuseum
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.hw09retrofitrijksmuseum.databinding.FragmentRetrofitBinding
 import com.google.android.material.divider.MaterialDividerItemDecoration
@@ -26,6 +29,8 @@ class RetrofitFragment : Fragment() {
 
     private var artObjectRequest: Call<ArtObject>? = null
 
+    private val currentArtList = mutableListOf<Art>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -42,10 +47,37 @@ class RetrofitFragment : Fragment() {
         with(binding) {
             recyclerView.adapter = adapter
             recyclerView.addItemDecoration(
-                MaterialDividerItemDecoration(requireContext(), MaterialDividerItemDecoration.VERTICAL))
+                object : RecyclerView.ItemDecoration() {
+                    override fun getItemOffsets(
+                        outRect: Rect,
+                        view: View,
+                        parent: RecyclerView,
+                        state: RecyclerView.State
+                    ) {
+                        outRect.bottom = INTERVAL_BETWEEN_ITEMS
+                    }
+                }
+//                MaterialDividerItemDecoration(requireContext(), MaterialDividerItemDecoration.VERTICAL)
+            )
+
+            toolbar
+                .menu
+                .findItem(R.id.action_search)
+                .actionView
+                .let { it as SearchView }
+                .setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String): Boolean {
+                        return false
+                    }
+
+                    override fun onQueryTextChange(newText: String): Boolean {
+                        adapter.submitList(currentArtList.filter { it.title.contains(newText) })
+                        return true
+                    }
+                })
         }
 
-        val retrofit = Retrofit.Builder()
+            val retrofit = Retrofit.Builder()
             .baseUrl("https://www.rijksmuseum.nl/")
             .addConverterFactory(GsonConverterFactory.create())
 //            .client(okHttpClient)
@@ -60,6 +92,7 @@ class RetrofitFragment : Fragment() {
                     override fun onResponse(call: Call<ArtObject>, response: Response<ArtObject>) {
                         if (response.isSuccessful) {
                             val artList = response.body()?.artObjects ?: return
+                            currentArtList.addAll(artList)
                             adapter.submitList(artList)
                         } else {
                             handleException(HttpException(response))
@@ -83,6 +116,9 @@ class RetrofitFragment : Fragment() {
 
     private fun handleException(e: Throwable) {
         Toast.makeText(requireContext(), e.message ?: "Something went wrong", Toast.LENGTH_SHORT).show()
+    }
+    companion object {
+        val INTERVAL_BETWEEN_ITEMS = 50
     }
 }
 
